65addd4d84cced522b2e5b1ebaeaa239631988ed
Moazzam Tanveer <ranamoazam954@gmail.com>
2025-04-24 10:00:23 +0500

    added video

--- Books-managment/backend/server.js
+++ Books-managment/backend/server.js
@@ -6,7 +6,7 @@ const bookRoutes = require("./routes/books");
 const connectDB = require("./config/db");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
